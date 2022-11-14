37f7316d5908ddfa27770fd67b81741e547a12c7
willMurrell <will.murrell908@gmail.com>
2022-11-14 14:13:32 +1300

    yeah added team template and stuff

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/static', express.static(path.join(__dirname, '/public/')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
 
 app.get('/', (request, response) => {
     response.render("home");
 });
  
-app.get('/', (request, response) => {
-    response.render('home')
-})
+
 app.listen(8080);    
